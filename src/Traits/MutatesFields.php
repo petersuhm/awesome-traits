1ab1da91ea1d49664fcf16e79085df328285b2a9
Peter Suhm <peter@suhm.dk>
2015-02-07 15:45:57 +0100

    Create MutatesFields.php

---
+++ src/Traits/MutatesFields.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AwesomeTraits;
+
+class MutatesFields
+{
+    public function __get($name)
+    {
+        // ...
+    }
+
+    public function __set($name, $value)
+    {
+        // ...
+    }
+}
